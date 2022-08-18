d2aa938c729bea4a51e50da97ead9feccd10945b
ezequielfajardo <109328643+EzequielFajardo@users.noreply.github.com>
2022-08-18 20:44:47 -0300

    autos

---
+++ js/products.js
@@ -0,0 +1,53 @@
+//array donde se cargarán los datos recibidos:
+let products = [];
+
+//función que recibe un array con los datos, y los muestra en pantalla a través el uso del DOM
+function showCategoriesList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i< array.length; i++) { 
+        let products = array[i]
+        htmlContentToAppend += `
+
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + products.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>` + products.name + " - " + products.currency + " " + products.cost +`</h4> 
+                        <p> `+ products.description + `</p>
+                        <small class="text-muted">` + products.soldCount + ` articulos</small>
+                        </div>
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+    }
+}
+
+
+/* 
+EJECUCIÓN:
+
+-Al cargar la página se llama a getJSONData() pasándole por parámetro la dirección para obtener el listado.
+-Se verifica el estado del objeto que devuelve, y, si es correcto, se cargan los datos en categoriesArray.
+-Por último, se llama a showCategoriesList() pasándole por parámetro categoriesArray.
+
+*/
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(LIST_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            products = resultObj.data.products;
+            showCategoriesList(products);
+        }
+    });
+});
